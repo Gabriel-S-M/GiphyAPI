2296bcc7292679a37dbe599e1eb8b57d5fb85da1
Gabriel Soares Marcondes <143465314+Gabriel-S-M@users.noreply.github.com>
2025-09-03 21:54:19 -0300

    Update

--- src/app/service/giph-service.ts
+++ src/app/service/giph-service.ts
@@ -16,8 +16,15 @@ export class GiphService {
     return this.http.get(`${this.baseurl}/trending`, { params });
   }
 
-  searchGifs(query:string, limit: number = 25, offset: number = 0, rating: string ='g') : Observable<any>{
-    const params = new HttpParams().set('apiKey', this.apiKey).set('q',query).set('limit', limit.toString()).set('offset',offset.toString()).set('rating',rating).set('bundle','messaging_non_clips');
+  searchGifs(query: string, limit: number = 25, offset: number = 0, rating: string = 'g'): Observable<any> {
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', query)
+      .set('limit', limit.toString())
+      .set('offset', offset.toString())
+      .set('rating', rating)
+      .set('bundle', 'messaging_non_clips');
+
     return this.http.get(`${this.baseurl}/search`, { params });
   }
 }
--- src/app/view/giph-detail/giph-detail.page.ts
+++ src/app/view/giph-detail/giph-detail.page.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-giph-detail',
   templateUrl: './giph-detail.page.html',
   styleUrls: ['./giph-detail.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class GiphDetailPage implements OnInit {
+  gif: any;
 
   constructor() { }
 
   ngOnInit() {
+    const navigation = window.history.state;
+    this.gif = navigation?.gif;
   }
 
 }
